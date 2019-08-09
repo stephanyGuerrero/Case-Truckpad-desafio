525ab959586aef2c2fc5e948d1e272c17612c9ac
Stephany <Stephany.blue0@gmail.com>
2019-08-09 17:06:35 -0300

    concluindo tela de inicio

--- src/App.vue
+++ src/App.vue
@@ -19,3 +19,15 @@ export default {
   })
 };
 </script>
+<style>
+body{
+  background-image: url("https://i0.wp.com/www.santafegrupo.com/wp-content/uploads/2017/08/Carretera-La-Paz-Centro-Malpaisillo-Grupo-Santa-Fe-HR.jpg?fit=1200%2C800&ssl=1");
+  background-size: cover;
+}
+
+#app {
+    background: transparent;
+  }
+
+</style>
+
--- src/components/Init.vue
+++ src/components/Init.vue
@@ -1,47 +1,77 @@
 <template>
   <div>
-    <v-layout align-center justify-center class="my-5">
-      <v-flex md6 xs10>
-        <v-img src="@/assets/logo-truckpad.png" alt />
-      </v-flex>
-    </v-layout>
-    <v-layout wrap align-center justify-center class="my-5">
-      <v-flex md12>
-<v-layout wrap align-center justify-center class="my-5">
-      <h2>Aqui pode Gerenciar seus Motoristas! </h2>
-       </v-layout>
-      </v-flex>
-      
-      <v-btn
-        data-aos="zoom-in"
-        data-aos-duration="2700"
-        class="my-5"
-        dark
-        large
-        href="/motorista"
-      >comecar</v-btn>
-      
-    </v-layout>
+    <v-flex class="ma-5" id="inicio">
 
-    <v-layout align-center justify-center row fill-height>
-      <v-flex class="my-5" md6>
-        <v-card id="info" color="#385F73" dark>
-          <v-card-text class="white--text">
-            <div class="headline mb-2">Cadastre!</div>
-          </v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
+      <v-layout align-center justify-center class="my-5">
+        <v-flex md6 xs10>
+          <v-img src="@/assets/logo-truckpad.png" alt />
+        </v-flex>
+      </v-layout>
 
-    <v-layout align-center justify-end row fill-height>
-      <v-flex class="my-5" md6>
-        <v-card id="info" color="#385F73" dark>
-          <v-card-text class="white--text">
-            <div class="headline mb-2">Edite!</div>
-          </v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
+      <v-layout wrap align-center justify-center class="my-5">
+
+        <v-flex xs11 md12>
+          <v-layout wrap align-center justify-center class="my-5">
+            <h2>Aqui você pode Gerenciar seus Motoristas!</h2>
+          </v-layout>
+        </v-flex>
+
+        <v-btn
+          id="comecar"
+          data-aos="zoom-in"
+          data-aos-duration="3000"
+          class="my-5 elevation-5"
+          dark
+          large
+          href="/motorista"
+        >começar
+        </v-btn>
+
+      </v-layout>
+
+      <v-layout align-center justify-center row fill-height>
+
+        <v-flex class="my-5 mx-5" md6 xs9>
+          <v-layout align-center justify-start row fill-height>
+            <div
+              class="elevation-5"
+              data-aos="zoom-out"
+              data-aos-duration="1000"
+              id="info"
+              dark>
+                <div id="texto" class="mx-5 my-2">Você consegue Cadastrar!</div>
+            </div>
+          </v-layout>
+        </v-flex>
+
+      </v-layout>
+
+      <v-layout align-center justify-center row fill-height>
+        <v-flex class="my-5" md6 xs9>
+          <v-layout align-center justify-end row fill-height>
+            <div
+              class="elevation-5 "
+              data-aos="zoom-out"
+              data-aos-duration="1800"
+              id="info"
+              dark>
+                <div id="texto" class="mx-5 my-2">Você consegue Editar seus cadastros!</div>
+            </div>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+
+      <v-layout wrap align-center justify-center class="my-5">
+
+        <v-flex class="mb-5" xs11 md12>
+          <v-layout wrap align-center justify-center>
+            <div id="nota"><h4 class="ma-5">Isto é um aplicativo teste, ou seja, apenas uma simulação de um gerenciador de motoristas :)</h4></div>
+          </v-layout>
+        </v-flex>
+
+      </v-layout>
+
+    </v-flex>
   </div>
 </template>
 <script>
@@ -58,6 +88,43 @@ export default {
 @import "./../../node_modules/aos/dist/aos.css";
 
 #info {
+  
+  height: 49px;
   width: 380px;
+  border-radius: 13px;
+  text-shadow: 2px 2px black;
+  color:white;
+  background-color:rgba(56, 95, 115, 0.712);
+}
+
+#texto{
+  font-size: 20px
+}
+
+#comecar {
+  border-radius: 20px;
+  width: 160px;
+  height: 70px;
+  font-size: 20px
+}
+
+#inicio {
+  background-color: rgba(255, 255, 0, 0.267);
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+h4{
+  color: azure;
+  text-shadow: 1px 1px black;
+}
+
+h2{
+  text-shadow: 2px 2px rgba(214, 214, 214, 0.767);
+}
+
+#nota{
+  border-radius: 18px;
+  border-color: rgb(112, 99, 99);
+  background-color: rgba(75, 107, 148, 0.493)
+
 }
 </style>
